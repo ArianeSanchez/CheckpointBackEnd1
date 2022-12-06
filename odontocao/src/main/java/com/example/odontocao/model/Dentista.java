0a0e283852d7af2ed3c51db7da3afde328067c4b
ArianeSanchez <ariane.sanchez1@gmail.com>
2022-12-06 19:33:47 -0300

    relação dentista one to many paciente

--- odontocao/src/main/java/com/example/odontocao/model/Dentista.java
+++ odontocao/src/main/java/com/example/odontocao/model/Dentista.java
@@ -1,9 +1,6 @@
 package com.example.odontocao.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Dentista {
@@ -13,6 +10,9 @@ public class Dentista {
     private String nome;
     private String sobrenome;
     private Integer matricula;
+
+    @OneToMany
+    private Paciente paciente;
 
     public Dentista() {
     }
