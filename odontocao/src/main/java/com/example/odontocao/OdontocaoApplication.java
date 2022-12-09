f6b9c45e66411c6bc129b68abfbd839c45179e21
Emanuel Nascimento <emanuel.nn@outlook.com>
2022-12-09 15:26:11 -0300

    Commit

--- odontocao/src/main/java/com/example/odontocao/OdontocaoApplication.java
+++ odontocao/src/main/java/com/example/odontocao/OdontocaoApplication.java
@@ -1,13 +1,19 @@
 package com.example.odontocao;
 
+import com.example.odontocao.model.Endereco;
+import com.example.odontocao.model.Paciente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OdontocaoApplication  {
-
+public class OdontocaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OdontocaoApplication.class, args);
+		Paciente paciente = new Paciente(1,"Emanuel","Nascimento","123456789","10/12/20220",
+				new Endereco(5,"Rua Almeida Cruz","71","Tubaguá","Imperial"));
+
+		System.out.println(paciente.toString());
+
 	}
 
 }
