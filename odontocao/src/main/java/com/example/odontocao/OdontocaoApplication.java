f992c389689830a9ad4dbbda072016e5a0e8d422
Emanuel Nascimento <emanuel.nn@outlook.com>
2022-12-09 14:29:31 -0300

    Correção de Bugs

--- odontocao/src/main/java/com/example/odontocao/OdontocaoApplication.java
+++ odontocao/src/main/java/com/example/odontocao/OdontocaoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OdontocaoApplication {
+public class OdontocaoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OdontocaoApplication.class, args);
